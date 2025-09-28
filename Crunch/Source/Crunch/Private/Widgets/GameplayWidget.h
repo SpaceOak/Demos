fdd51cd7a88f9b41ed1f3a2d5bc5e84c95aa7003
Penis Buba <qvest777@yandex.ru>
2025-09-28 16:07:51 +0200

    Public. Inpelemt mana and health widget

--- Crunch/Source/Crunch/Private/Widgets/GameplayWidget.h
+++ Crunch/Source/Crunch/Private/Widgets/GameplayWidget.h
@@ -13,10 +13,15 @@ UCLASS()
 class UGameplayWidget : public UUserWidget
 {
 	GENERATED_BODY()
+public:
+	virtual void NativeConstruct() override;
 private:
 	UPROPERTY(meta=(BindWidget))
 	class UValueGauge* HealthBar;
 
 	UPROPERTY(meta=(BindWidget))
 	class UValueGauge* ManaBar;
+
+	UPROPERTY()
+	class UAbilitySystemComponent* OwnerAbilitySystemComponent;
 };
